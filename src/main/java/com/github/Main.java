b61d69bcd278e55ebc3a26bbc237bdbda69d5070
erfandavoodinasr <davoodinasr.erfan@gmail.com>
2024-12-16 17:20:50 +0330

    move to web application

--- src/main/java/com/github/Main.java
+++
@@ -1,9 +0,0 @@
-package com.github;
-
-import com.github.ui.LoginPage;
-
-public class Main {
-    public static void main(String[] args) {
-        LoginPage.login();
-    }
-}
--- src/main/java/com/github/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.github.service;
-
-import com.github.base.service.BaseService;
-import com.github.model.User;
-
-public interface UserService extends BaseService<User, Integer> {
-    User findByPhoneNUmber(String phoneNumber);
-}
